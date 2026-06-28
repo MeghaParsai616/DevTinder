208110b3c498961cc23c620fa7a36a5c4adf2acb
sanskriti parsai <sanskritiparsai@sanskritis-MacBook-Air.local>
2026-06-28 21:57:11 +0530

    CRUD

--- src/app.js
+++ src/app.js
@@ -2,25 +2,74 @@ console.log("Beleiver...........🚀");
 const express = require("express");
 const connectDB = require("./config/database");
 // const { adminAuth, userAuth } = require("./middlewares/auth");
-const User = require("./models/user")
+const User = require("./models/user");
+const user = require("./models/user");
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
+app.post("/signup", async (req, resp) => {
 
-app.post("/signup", async(req, resp)=>{
-  console.log(req.body)
-    // creating an new instance of user model 
-    const user = new User(req.body);
+    // validation of data 
 
-    try{
-         await user.save();
-    resp.send("User Added successfully")
-    }catch(err){
-       resp.status(400).send("error saving the user")
+    // encrypt the password
+
+    
+  console.log(req.body);
+  // creating an new instance of user model
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    resp.send("User Added successfully");
+  } catch (err) {
+    resp.status(400).send("error saving the user....", err.message);
+  }
+});
+
+// Feed API : GET / feed get all the users from the database
+
+app.get("/feed", async (req, resp) => {
+  try {
+    const users = await User.find({});
+    resp.send(users);
+    if (users.length === 0) {
+      resp.status(404).send("User Not Found");
+    } else {
+      resp.send(users);
     }
-   
+  } catch (err) {
+    resp.status(400).send("something wwend wrong");
+  }
+});
 
-})
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (err) {}
+});
+
+app.patch("/user/:userId", async (req, resp) => {
+  //const userId = req.body.userId;
+    const userId = req.params?.userId;
+    const data = req.body;
+
+  try {
+    const ALLOWED_UPDATES = ["about", "gender", "age"];
+    const isUpdateAllowed = Object.keys(data).every((key) =>
+      ALLOWED_UPDATES.includes(key),
+    );
+
+    if (!isUpdateAllowed) throw new Error("Update not allowed")
+
+    await User.findByIdAndUpdate({ _id: userId }, req.body);
+    resp.send("user Updated successfully");
+  } catch (err) {
+    resp.status(400).send("Could not Update :  " +  err?.message);
+  }
+});
 
 connectDB()
   .then(() => {
@@ -32,15 +81,6 @@ connectDB()
   .catch((err) => {
     console.log("error with DB connection..", err);
   });
-
-
-
-
-
-
-
-
-
 
 // app.use("/admin", adminAuth);
 
--- src/models/user.js
+++ src/models/user.js
@@ -3,12 +3,18 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required: true,
+    minLength: 4,
+    maxLength:14
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    required: true,
+    unique: true,
+    trim: true,
   },
   password: {
     type: String,
@@ -19,6 +25,16 @@ const UserSchema = new mongoose.Schema({
   gender: {
     type: String,
   },
+  about:{
+    type: String,
+    default: "This is default String Val "
+  },
+  skills:{
+    type: [String]
+  }
+},
+{
+timestamps : true
 });
 
 module.exports = mongoose.model("User", UserSchema);
